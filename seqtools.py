da413b66e9415d6a9eff5b9b90538a398d16b722
Jackson Shartzer <jacksonshartzer@gmail.com>
2025-11-07 10:37:24 -0500

    Pass Doctests

--- seqtools.py
+++ seqtools.py
@@ -1,13 +1,13 @@
 """
->>> myseq = Seqtools([1, 2, 'a', 7, ('x', 'y'), 'yup', 11.3, True'])
+>>> myseq = Seqtools([1, 2, 'a', 7, ('x', 'y'), 'yup', 11.3, True])
 >>> myseq.data()
-[1, 2, 'a', 7, ('x', 'y'), 'yup', 11.3, True']
+[1, 2, 'a', 7, ('x', 'y'), 'yup', 11.3, True]
 >>> myseq.nums()
 [1, 2, 7, 11.3]
 >>> myseq.seqs()
 ['a', ('x', 'y'), 'yup']
 >>> myseq.others()
-[True']
+[True]
 """
 
 class Seqtools():
@@ -22,8 +22,8 @@ class Seqtools():
         numsList = []
         numTypes = (int, float)
         for item in self.list:
-            if isinstance(self.list[item], (numTypes)):
-                numsList.append(self.list[item])
+            if isinstance(item, (numTypes)) and not isinstance(item, bool):
+                numsList.append(item)
             else:
                 continue
         return numsList
@@ -32,18 +32,18 @@ class Seqtools():
         seqsList = []
         seqTypes = (str, tuple, list, dict, set)
         for item in self.list:
-            if isinstance(self.list[item], (seqTypes)) in seqTypes:
-                seqsList.append(self.list[item])
-            else:
-                continue
+            if isinstance(item, (seqTypes)):
+                seqsList.append(item)
         return seqsList
 
     def others(self):
         othersList = []
-        typesList = (str, list, tuple, dict, set)
+        typesList = (str, list, tuple, dict, set, int, float)
         for item in self.list:
-            if not isinstance(self.list[item], (typesList)):
-                othersList.append(self.list[item])
+            if not isinstance(item, (typesList)):
+                othersList.append(item)
+            elif isinstance(item, bool):
+                othersList.append(item)
             else:
                 continue
         return othersList
